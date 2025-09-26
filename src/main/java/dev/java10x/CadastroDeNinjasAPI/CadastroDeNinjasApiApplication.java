ee9706d702370730b2df454c8bcda679d655cfde
Seu Nome <lucasfermagalhaes@gmail.com>
2025-09-26 14:49:32 -0300

    initial

---
+++ src/main/java/dev/java10x/CadastroDeNinjasAPI/CadastroDeNinjasApiApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjasAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeNinjasAPI/CadastroDeNinjasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjasAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
