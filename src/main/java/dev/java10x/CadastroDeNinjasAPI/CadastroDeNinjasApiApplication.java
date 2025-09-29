50918d492e15a7ea139ab7b0a7c4a1671a8026a6
Seu Nome <lucasfermagalhaes@gmail.com>
2025-09-29 15:14:48 -0300

    .

--- src/main/java/dev/java10x/CadastroDeNinjasAPI/CadastroDeNinjasApiApplication.java
+++ src/main/java/dev/java10x/CadastroDeNinjasAPI/CadastroDeNinjasApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CadastroDeNinjasApiApplication {
 
-	public static void main(String[] args) {
+	public  static void main(String[] args) {
 		SpringApplication.run(CadastroDeNinjasApiApplication.class, args);
 	}
 
