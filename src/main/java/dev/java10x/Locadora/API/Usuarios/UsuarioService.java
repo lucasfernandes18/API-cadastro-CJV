7d44f8819e98d784a79abb1578332a149f70ab3f
Seu Nome <lucasfermagalhaes@gmail.com>
2025-11-10 14:40:53 -0300

    refactor: adiciona final a injeção das minhas dependências e também o thymeleaf para a rendenicação

--- src/main/java/dev/java10x/Locadora/API/Usuarios/UsuarioService.java
+++ src/main/java/dev/java10x/Locadora/API/Usuarios/UsuarioService.java
@@ -9,8 +9,8 @@ import java.util.Optional;
 @Service
 public class UsuarioService {
 
-    private UsuarioRepository usuarioRepository;
-    private UsuarioMapper usuarioMapper;
+    private final UsuarioRepository usuarioRepository;
+    private final UsuarioMapper usuarioMapper;
 
     public UsuarioService(UsuarioRepository usuarioRepository, UsuarioMapper usuarioMapper) {
         this.usuarioRepository = usuarioRepository;
